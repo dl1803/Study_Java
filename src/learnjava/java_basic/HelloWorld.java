2623052b0acaf5bd284d4cae81d617047e0aac7f
dl1803 <loitran0990@gmail.com>
2026-01-30 09:22:37 +0700

    init project

---
+++ src/learnjava/java_basic/HelloWorld.java
@@ -0,0 +1,27 @@
+package learnjava.java_basic;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		/*
+		 * Goi y code -> Ctrl + Space - vd : main + ctrl + space + enter / sysout + ___
+		 * ....
+		 */
+		/*
+		 * Chay chuong trinh : C1: Chuot phai tep java -> Run as -> 1 C2: Chuot phai tep
+		 * java -> properties -> mo tren Window Explorer -> go cmd lên thanh timkiem de
+		 * mo cd -> go java <ten tep java.java>
+		 */
+		/*
+		 * Format code : C1: chon vung code can format -> chuot phai -> source -> format
+		 * C2(uu tien): Ctrl + S -> auto format (can cau hinh: Window → Preferences ->
+		 * Java → Editor → Save Actions -> Perform the selected actions on save Format
+		 * source code )
+		 */
+
+		System.out.println("Hello World with Java! 👋");
+		System.out.println("Hello World with Java! 👋");
+		System.out.println("Hello World with Java! 👋");
+		System.out.println("Hello World with Java! 👋");
+
+	}
+}
